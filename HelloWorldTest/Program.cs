613522f36645f96cfaa6fb4a6b019055e460ff54
JustRynx <126393310+JustRynx@users.noreply.github.com>
2023-02-26 13:04:07 +0000

    hello world test

---
+++ HelloWorldTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorldTest{
+
+    public class Program{
+
+        
+        public static void Main(string[] args){
+
+            int a = 1, b = 2;
+
+            Console.WriteLine("Hello World~, This is only a test!");
+
+            Console.WriteLine("Total: " + (a + b));
+        }
+
+      
+
+    }
+}
